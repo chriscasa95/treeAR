6e702ab5af45286bc79a8b762b32f4897d1f087c
Christoph Casagranda <christoph.casagranda@karlstorz.com>
2023-01-10 10:45:12 +0700

    Develop video capturing

--- PlantGardener/app/ImageUploader.py
+++ PlantGardener/app/ImageUploader.py
@@ -1,9 +1,8 @@
 import cv2
 import base64
 import json
-import os
+import time
 import sys
-import numpy as np
 
 from cv2 import Mat
 from TargetAPI import TargetAPI
@@ -14,32 +13,48 @@ MEGABYTE = 1024 * 1024
 class ImageUploader(TargetAPI):
     def upload_image(self, img_path: str, img_name: str, width: str):
         img = cv2.imread(img_path)
+        img_name = self.__generate_img_name(img_name, 0)
+
+        self.__upload(img, img_name, width)
+
+    # def upload_multiple_images(self, img_paths: list[str]):
+    #     pass
+
+    # def upload_images_from_folder(self, folder_path: str):
+    #     pass
+
+    def upload_video(
+        self, video_path: str, img_name: str, width: str, number_of_images: int
+    ):
+        extracted_frames = self.__extract_farmes(video_path, number_of_images)
+
+        for idx, img in enumerate(extracted_frames):
+            unique_img_name = self.__generate_img_name(img_name, idx)
+            self.__upload(img, unique_img_name, width)
+
+    # def get_image_information(self, target_id: str):
+    #     pass
+
+    # def update_image(self, target_id: str, image_path: str):
+    #     pass
+
+    # def delete_image(self, target_id: str):
+    #     pass
+
+    def __upload(self, img: Mat, img_name: str, width: str):
+        print(f"upload: {img_name}")
 
         img_compressed = self.__compress_image(img)
 
         html_body = self.__generate_body(img_compressed, img_name, width)
 
-        self._post(html_body)
+        # self._post(html_body)
 
-        print(f"uploaded: {img_name}")
+    def __generate_img_name(self, img_name: str, counter: int) -> str:
+        # ts stores the time in seconds
+        ts = int(time.time())
 
-    def upload_multiple_images(self, img_paths: list[str]):
-        pass
-
-    def upload_video(self, video_path: str):
-        pass
-
-    def upload_images_from_folder(self, folder_path: str):
-        pass
-
-    def get_image_information(self, target_id: str):
-        pass
-
-    def update_image(self, target_id: str, image_path: str):
-        pass
-
-    def delete_image(self, target_id: str):
-        pass
+        return f"{img_name}_{ts}_{counter}"
 
     def __get_jpg_byte_size(self, image: Mat) -> int:
 
@@ -70,6 +85,33 @@ class ImageUploader(TargetAPI):
         dictonary = {"name": name, "width": width, "image": jpg_as_b64}
 
         return json.dumps(dictonary, ensure_ascii=False, indent=4)
+
+    def __extract_farmes(self, video_path: str, number_of_images: int) -> list[Mat]:
+        captured_frames = []
+        # Start capturing the feed
+        cap = cv2.VideoCapture(video_path)
+
+        number_of_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) - 1
+        capture_nth_frame = number_of_frames / number_of_images
+        print(
+            f"Video has {number_of_frames}; Capture every {capture_nth_frame}th frame\nStart capturing..."
+        )
+
+        count = 0
+        while cap.isOpened():
+            ret, frame = cap.read()
+
+            if ret:
+                # cv2.imwrite("frame{:d}.jpg".format(count), frame)
+                captured_frames.append(frame)
+                count += capture_nth_frame  # i.e. at 30 fps, this advances one second
+                cap.set(cv2.CAP_PROP_POS_FRAMES, count)
+            else:
+                cap.release()
+                break
+
+        print("Finished capturing!\n")
+        return captured_frames
 
 
 if __name__ == "__main__":
