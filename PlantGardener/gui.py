71d638f3fc9d407c00978b2952543da2c607a086
Christoph Casagranda <christoph.casagranda@karlstorz.com>
2023-01-11 10:49:14 +0700

    First GUI version

--- PlantGardener/gui.py
+++ PlantGardener/gui.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+import json
 import ctypes
 import platform
 
@@ -8,6 +9,70 @@ sg.theme("DarkBlue")  # Add a touch of color
 # All the stuff inside your window.
 
 import layout.upload_layout as upload
+from app.ImageUploader import ImageUploader
+
+
+def __get_metadata(meta_path: str) -> str:
+    metadata = ""
+
+    if meta_path:
+        with open(meta_path) as fd:
+            metadata = json.dumps(json.load(fd))
+
+    return metadata
+
+
+def handle_submit(event, values) -> tuple[bool, str]:
+
+    if not values["-access_key-"]:
+        return [False, "No Server Access Key!"]
+    if not values["-secret_key-"]:
+        return [False, "No Server Secret Key!"]
+    if not values["-name-"]:
+        return [False, "No Image Name!"]
+    if not values["-width-"]:
+        return [False, "No Image Width!"]
+
+    if values["image_mode"]:
+        if not values["-image_path-"]:
+            return [False, "No Image-Path!"]
+
+        uploader = ImageUploader(values["-access_key-"], values["-secret_key-"])
+        metadata = __get_metadata(values["-meta_path-"])
+
+        uploader.upload_image(
+            values["-image_path-"], values["-name-"], values["-width-"], metadata
+        )
+
+    elif values["folder_mode"]:
+        if not values["-folder_path-"]:
+            return [False, "No Folder-Path!"]
+
+        uploader = ImageUploader(values["-access_key-"], values["-secret_key-"])
+        metadata = __get_metadata(values["-meta_path-"])
+
+        uploader.upload_images_from_folder(
+            values["-folder_path-"], values["-name-"], values["-width-"], metadata
+        )
+
+    elif values["video_mode"]:
+        if not values["-video_path-"]:
+            return [False, "No Video-Path!"]
+
+        uploader = ImageUploader(values["-access_key-"], values["-secret_key-"])
+        metadata = __get_metadata(values["-meta_path-"])
+
+        number_of_images = 30
+
+        uploader.upload_video(
+            values["-video_path-"],
+            values["-name-"],
+            values["-width-"],
+            number_of_images,
+            metadata,
+        )
+
+    return [True, "\nTask finished!\n\n"]
 
 
 update_layout = [
@@ -29,11 +94,11 @@ delete_layout = [
 input_layout = [
     [
         sg.Text("Server Access Key: "),
-        sg.InputText(),
+        sg.InputText(k="-access_key-"),
     ],
     [
         sg.Text("Server Secret Key:  "),
-        sg.InputText(),
+        sg.InputText(k="-secret_key-"),
     ],
     [
         sg.Text("Mode:                    "),
@@ -128,6 +193,10 @@ window = sg.Window("Plantgardener", layout)
 while True:
     event, values = window.read()
 
+    window["-UPLOAD_IMAGE_LAYOUT-"].update(visible=True)
+    window["-UPLOAD_FOLDER_LAYOUT-"].update(visible=True)
+    window["-UPLOAD_VIDEO_LAYOUT-"].update(visible=True)
+
     if event == "Upload":
         print("HI")
 
@@ -135,38 +204,8 @@ while True:
         break
     # window["-FOLDERNAME-"].hide_row()
 
-    elif event == "Clear History":
-        print("CLEAR")
-
-    if values["upload_mode"]:
-        print("upload_mode")
-        window["-UPLOAD_LAYOUT-"].unhide_row()
-        window["-UPDATE_LAYOUT-"].hide_row()
-        window["-GET_LAYOUT-"].hide_row()
-        window["-DELETE_LAYOUT-"].hide_row()
-
-    elif values["update_mode"]:
-        print("update_mode")
-        window["-UPLOAD_LAYOUT-"].hide_row()
-        window["-UPDATE_LAYOUT-"].unhide_row()
-        window["-GET_LAYOUT-"].hide_row()
-        window["-DELETE_LAYOUT-"].hide_row()
-
-        window["-UPDATE_LAYOUT-"]._visible = True
-
-    elif values["get_mode"]:
-        print("get_mode")
-        window["-UPLOAD_LAYOUT-"].hide_row()
-        window["-UPDATE_LAYOUT-"].hide_row()
-        window["-GET_LAYOUT-"].unhide_row()
-        window["-DELETE_LAYOUT-"].hide_row()
-
-    elif values["delete_mode"]:
-        print("delete_mode")
-        window["-UPLOAD_LAYOUT-"].hide_row()
-        window["-UPDATE_LAYOUT-"].hide_row()
-        window["-GET_LAYOUT-"].hide_row()
-        window["-DELETE_LAYOUT-"].unhide_row()
+    elif event == "-submit-":
+        handle_submit(event, values)
 
     if values["image_mode"]:
         window["-UPLOAD_IMAGE_LAYOUT-"].unhide_row()
--- PlantGardener/layout/upload_layout.py
+++ PlantGardener/layout/upload_layout.py
@@ -3,7 +3,7 @@ import PySimpleGUI as sg
 __image_layout = [
     [sg.T("Select Image:")],
     [
-        sg.In(sg.user_settings_get_entry("-editor program-", ""), k="-EDITOR PROGRAM-"),
+        sg.In(sg.user_settings_get_entry("-editor program-", ""), k="-image_path-"),
         sg.FileBrowse(file_types=(("img", ".jpg .JPG"),)),
     ],
 ]
@@ -12,11 +12,11 @@ __image_layout = [
 __folder_layout = [
     [sg.T("Select Folder:")],
     [
-        sg.Combo(
+        sg.In(
             sorted(sg.user_settings_get_entry("-folder names-", [])),
             # default_value=sg.user_settings_get_entry("-demos folder-", "get_demo_path"),
-            size=(44, 1),
-            key="-FOLDERNAME-",
+            size=(45, 1),
+            k="-folder_path-",
         ),
         sg.FolderBrowse("Browse", target="-FOLDERNAME-"),
     ],
@@ -26,30 +26,38 @@ __folder_layout = [
 __video_layout = [
     [sg.T("Select Video:")],
     [
-        sg.In(sg.user_settings_get_entry("-editor program-", ""), k="-EDITOR PROGRAM-"),
+        sg.In(sg.user_settings_get_entry("-editor program-", ""), k="-video_path-"),
         sg.FileBrowse(file_types=(("video", ".mp4 .MP4"),)),
     ],
 ]
 
+__submit_layout = [
+    [sg.B("Submit", button_color="red", k="-submit-")],
+]
 
 input_layout = [
     [sg.T("Select Metadata:")],
     [
-        sg.In(sg.user_settings_get_entry("-explorer program-"), k="-EXPLORER PROGRAM-"),
+        sg.In(sg.user_settings_get_entry("-explorer program-"), k="-meta_path-"),
         sg.FileBrowse(file_types=(("metadata", "*.json *.JSON"),)),
     ],
     [
         sg.Text("Name: "),
     ],
     [
-        sg.InputText(),
+        sg.InputText(k="-name-"),
     ],
     [
         sg.Text("Width: "),
     ],
     [
-        sg.InputText(default_text=1),
+        sg.InputText(
+            default_text=1,
+            k="-width-",
+        ),
     ],
+    [sg.HSeparator()],
+    [sg.Col(__submit_layout, element_justification="c")],
 ]
 
 
@@ -67,7 +75,7 @@ layout = [
         sg.R("Video", enable_events=True, group_id="upload_mode", k="video_mode"),
     ],
     [sg.Col(__image_layout, k="-UPLOAD_IMAGE_LAYOUT-")],
-    [sg.Col(__folder_layout, k="-UPLOAD_FOLDER_LAYOUT-")],
-    [sg.Col(__video_layout, k="-UPLOAD_VIDEO_LAYOUT-")],
+    [sg.Col(__folder_layout, k="-UPLOAD_FOLDER_LAYOUT-", visible=False)],
+    [sg.Col(__video_layout, k="-UPLOAD_VIDEO_LAYOUT-", visible=False)],
     [sg.Col(input_layout, k="-UPLOAD_INPUT_LAYOUT-")],
 ]
